41dbddb4f41b86ce1af872065c0f931b650a033c
Catursn <42853990+Catursn@users.noreply.github.com>
2021-04-05 12:05:21 +0700

    main

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>BPSDM PUPR</title>
+        <link rel='icon' href="{{asset('images/Logo_PU_(RGB).png')}}">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    </head>
+    <body>
+    <!-- <div style="background-image: url('images/batikpuscreen.jpg'); background-size: 100% auto;"> -->
+    <!-- NABAR START -->
+            <!-- <p><img src="images/Artboard – 7.png" alt=""></p> -->
+            <!-- <div class="col-lg-12 col-md-9 col-sm-9">
+            <a href=""><img src="images/Artboard – 7.png" alt="" style="width:100%; height:auto"></a>
+            </div> -->
+            <!-- <a class="navbar-brand" href="#"><img src="images/Artboard – 7.png" alt="" style="width:100%; height:auto"></a> -->
+            <!-- <p><img src="images/Artboard – 7.png" alt="" style="width:60%; height:auto"></p> -->
+        <!-- <div class="container"> -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+        
+    <!-- NAVBAR END -->
+    <!-- CAROUSEL START -->
+        <div>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+
+                <div class="item active">
+                    <img src="images/carousel/block_601762f410fc9.png" alt="Los Angeles" style="width:100%;">
+                    <div class="carousel-caption">
+                    <h3>Los Angeles</h3>
+                    <p>LA is always so much fun!</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="images/carousel/block_601762f410fc9.png" alt="Chicago" style="width:100%;">
+                    <div class="carousel-caption">
+                    <h3>Chicago</h3>
+                    <p>Thank you, Chicago!</p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/carousel/block_601762f410fc9.png" alt="New York" style="width:100%;">
+                    <div class="carousel-caption">
+                    <h3>New York</h3>
+                    <p>We love the Big Apple!</p>
+                    </div>
+                </div>
+            
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    <!-- CAROUSEL END -->
+    </div>
+    </body>
+    <script>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/3.5.1.jquery.min.js"></script>
+        <script src="js/js/3.4.1.bootstrap.min.js"></script>
+        <script src="js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </script>
+</html>
